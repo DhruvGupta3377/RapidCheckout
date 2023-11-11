6280135cc4777763a9143ff3db328e6be6b07681
DhruvGupta3377 <dhruvgupta3377@gmail.com>
2023-11-11 12:43:18 +0530

    removed window reload after removing element

---
+++ frontend/constant.js
@@ -0,0 +1,3 @@
+// constants.js
+const BASE_URL = "http://127.0.0.1:8000";
+export default BASE_URL;
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,48 +1,47 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import BASE_URL from "../../constant";
 
 const Cart = () => {
+  async function fetchData() {
+    const apiUrl = `${BASE_URL}/api/getcartitems/`;
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setItems(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
   const [items, setItems] = useState([]);
   useEffect(() => {
-    const apiUrl = "http://127.0.0.1:8000/api/getcartitems/";
     console.log("fetching cart items");
-    async function fetchData() {
-      try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
     // Call the async function
     fetchData();
   }, []);
 
   const removeitemhandler = (itemid) => {
-    window.location.reload(false);
-    const url = "http://127.0.0.1:8000/api/removecartitem/";
-
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Token edf68cf84cd806ad12ae51271a68abd107dcc391",
-      },
-      body: JSON.stringify({ id: itemid }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
+    async function removeitem() {
+      const url = `${BASE_URL}/api/removecartitem/`;
+      try {
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token edf68cf84cd806ad12ae51271a68abd107dcc391",
+          },
+          body: JSON.stringify({ id: itemid }),
+        });
+        await fetchData();
+      } catch (error) {
         console.error("Error fetching data:", error);
-      });
+      }
+    }
+    removeitem();
   };
 
   return (
